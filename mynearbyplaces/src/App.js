778ff0c8256bb3d11310c65a8730b1c26d040b30
FatimahAldhamen <Aldhamen@email.arizona.edu>
2021-04-09 01:20:07 +0500

    .

--- mynearbyplaces/src/App.js
+++ mynearbyplaces/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Update from "./routes/Update";
 import Details from "./routes/Details";
