8c1317b89d9ff6fd8503e51c103c945a5b7beeb5
fatimahaldhamen <aldhamen@email.arizona.edu>
2021-05-13 01:59:04 +0500

    fixed states

--- mynearbyplaces/src/routes/AddPlace.jsx
+++ mynearbyplaces/src/routes/AddPlace.jsx
@@ -1,38 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const submitHandler = async (event) => {
-    const URL = "https://fatimahaldhamen-mynearbyplace.herokuapp.com/add/place";
-    event.preventDefault();
-    await fetch(URL, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            name: document.getElementById("name").value,
-            place: document.getElementById("place").value,
-            location: document.getElementById("location").value
-        })
-    })
-        .then((res) => res.json())
-        .then((data) => {
-            alert("Place Added Successfully!");
-        })
-    window.location.reload();
-}
 
 const AddPlace = () => {
+    const [details, setDetails] = useState({ name: '', place: '', location: '' })
+    const submitHandler = async (event) => {
+        const URL = "https://fatimahaldhamen-mynearbyplace.herokuapp.com/add/place";
+        event.preventDefault();
+        await fetch(URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                name: details.name,
+                place: details.place,
+                location: details.location
+            })
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                alert("Place Added Successfully!", data);
+            })
+        window.location.reload();
+    }
     return (
         <div className="mb-4">
             <h1 className="text-center my-4 font-weight-light">Add Places</h1>
             <form onSubmit={submitHandler} className="custom-form">
                 <div className="form-row">
                     <div className="col">
-                        <input id="name" type="text" className="form-control" placeholder="Name" required />
+                        <input id="name" type="text" className="form-control" placeholder="Name" onChange={e => setDetails({ ...details, name: e.target.value })} value={details.name} required />
                     </div>
                     <div className="col">
-                        <input id="location" type="text" className="form-control" placeholder="Location" required />
+                        <input id="location" type="text" className="form-control" placeholder="Location" onChange={e => setDetails({ ...details, location: e.target.value })} value={details.location} required />
                     </div>
                     <div className="col">
-                        <input id="place" type="text" className="form-control" placeholder="place" required />
+                        <input id="place" type="text" className="form-control" placeholder="place" onChange={e => setDetails({ ...details, place: e.target.value })} value={details.place} required />
                     </div>
                     <button className="btn btn-primary">Add</button>
                 </div>
